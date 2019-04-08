00c80957aeae596c6b9fd1d22c75b06e5729d02c
turovets90 <a.turovets90@gmail.com>
2019-04-08 19:58:49 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -94,6 +94,90 @@ $(document).ready(function(){
     }
 
 
+
+    $(window).resize(function(){
+        var header = $('header').outerHeight();
+        $(window).scroll(function(){
+            if ($(window).scrollTop() > header) {
+                $('header').addClass('fixed');
+            } else {
+                $('header').removeClass('fixed');
+            }
+        });
+
+        $('main.page').css({
+            'padding-top':header+'px'
+        });
+
+    });
+    $(window).resize();
+
+
+    $('.c_nav li').each(function(){
+        var c_nav_li =$(this);
+        var arrow =$(this).find('.arrow');
+        var sub_menu =$(this).find('.sub_menu');
+        $(arrow).click(function(){
+            $(c_nav_li).toggleClass('open');
+            $(sub_menu).slideToggle();
+        });
+    });
+
+
+    $('.filter_nav .filter_group').each(function(){
+        var filter_group=$(this);
+        var filter_title=$(this).find('.filter_title');
+        var filter_group_body=$(this).find('.filter_group_body');
+        $(filter_title).click(function(){
+            $(filter_group).toggleClass('open');
+            $(filter_group_body).slideToggle();
+        });
+    });
+
+
+    $('.catalog_mobile_toggler').click(function(){
+        $(this).toggleClass('act');
+        $(this).next().slideToggle();
+    });
+
+    $('.filter_mobile_toggler').click(function(){
+        $(this).toggleClass('act');
+        $(this).next().slideToggle();
+    });
+
+
+    if($(window).innerWidth() < 1200){
+
+        if($(".brands_slider .item").length >3){
+            $('.brands_slider').slick({
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                arrows: true,
+                dots:false,
+                infinite:false,
+                responsive: [
+                    {
+                        breakpoint: 768,
+                        settings: {
+                            slidesToShow: 2,
+                        }
+                    },
+
+                    {
+                        breakpoint: 375,
+                        settings: {
+                            slidesToShow: 1,
+                        }
+                    }
+                ]
+            });
+        }
+
+    }
+
+
+
+
 /*
         $('.hamburger').click(function () {
             $('.main_menu').toggleClass('open');
